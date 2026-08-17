d01139c254c885b743e748841deffaec5da6395e
Bkumarsh <152422371+Bkumarsh@users.noreply.github.com>
2026-08-17 21:20:20 +0530

    isse resolve

--- .github/workflows/ascii-script.sh
+++ .github/workflows/ascii-script.sh
@@ -1,4 +1,4 @@
-sudo apt-get update && sudo apt-get install -y cowsay
+sudo apt-get update -y && sudo apt-get install -y
 cowsay -f elephant "I am an elephant" >> elephant.txt
 cat elephant.txt
 ls -ltra
